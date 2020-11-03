b428736ab6d72d208153685d9d3d36b0902de634
Kofi Amoah <KofiAmoah@users.noreply.github.com>
2020-11-02 22:10:04 -0800

    Update detect.non-causal.py

--- src/detect.non-causal.py
+++ src/detect.non-causal.py
@@ -36,21 +36,16 @@ def main(argv):
 
 	#store the snv to be tested
 	testing = defaultdict(set) #(chrm.pos.strd,exon): set of lines
-#	means = defaultdict(list) #(chrm.pos.strd,exon): list of peakSiMean
-#	stdev = defaultdict(list) #(chrm.pos.strd,exon): list of peakSiStdev
 	if opts.r:
 		with open(opts.r) as f:
 			for ll in f:
 				ll = ll.strip()
 				l = ll.split()
 				testing[(l[0],l[3])].add(ll)
-#				means[(l[0],l[3])].append(float(l[13]))
-#				stdev[(l[0],l[3])].append(float(l[14]))
 
 	#data[case]: [het-good,het-bad,hom-good,hom-bad]; good: si->1, bad: si->0 
 	zero = lambda:{'hetgood':0, 'hetbad':0, 'homgood':0, 'hombad':0}
 	data = defaultdict(zero)
-#	hetgood,hetbad,homgood,hombad = 0, 0, 0, 0
 	for ff in glob.glob('{}/*2/{}*'.format(opts.i,opts.s)):
 		with open(ff) as f:
 			for l in f:
@@ -58,7 +53,6 @@ def main(argv):
 				l = l.strip().split('\t')
 				if (l[0],l[2]) not in testing: continue
 				gt,source,nt = l[1].split('|')
-#				data[(l[0],l[3])][gt].append(float(l[-1]))
 				si = float(l[-1])
 				if si > 0.5:
 					if gt in ('0/1','1/0'): #het
@@ -80,36 +74,10 @@ def main(argv):
 			hetbad = data[case]['hetbad']
 			hombad = data[case]['hombad']
 			oddsratio, pvalue = stats.fisher_exact([[hetgood, hetbad], [homgood, hombad]])
-			#if pvalue <= 0.05: continue
-			#exicute below if no bias based on gt => GOOD!
+			#execute below if no bias based on gt => GOOD!
 			for x in testing[case]:
 				#out.write(x)
 				out.write('{}\t{}\n'.format(x,pvalue))
-
-	#generate 2x2 
-	#res[case]: [het-good,het-bad,hom-good,hom-bad]; good: si->1, bad: si->0 
-#	for case in data.iterkeys():
-#		mu = np.mean(means[case])
-#		sigma = np.mean(stdev[case])
-#		norm = np.random.normal(mu, sigma, 1000)
-#		het = data[case]['0/1'] + data[case]['1/0']
-#		hom = data[case]['0/0'] + data[case]['1/1']
-#		ht_t, ht_p = stats.ttest_ind(het, norm, equal_var = False)
-#		hm_t, hm_p = stats.ttest_ind(hom, norm, equal_var = False)
-#		if ht_p < 0.05 and hm_p < 0.05:
-#			print 'uh-oh1',case
-#			print 'mu:', mu
-#			print 'het:',het,ht_p
-#			print 'hom:',hom,hm_p
-#			sys.exit()
-#		elif ht_p < 0.05: #ht is deviate from mu Si => bad group
-#			
-#		elif hm_p < 0.05: #hm is deviate from mu Si => bad group
-#		else: #both are insignificant...
-#			print 'uh-oh2',case
-#			print 'mu:', mu
-#			print 'het:',het,ht_p
-#			print 'hom:',hom,hm_p
 
 	print("--- %s seconds ---" % (time.time() - start_time))
 	print 'DONE!', strftime('%a, %d %b %Y %I:%M:%S')
